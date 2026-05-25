5f373a867a5cfdf47569e9a19e37d5cf1dde68d8
Yggdrasil-Lrss <lucas.renato@nave.org.br>
2026-05-25 09:08:10 -0300

    r

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -3,3 +3,15 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+import { View, Text, StyleSheet, Pressable} from 'react-native';
+import Botao from '../styles/main';
+
+export default function AtvBotaoAcaoExterna({comando}) {
+    return(<View>
+        <Pressable style={estilo.button} 
+        comando=[() => console.log("comando")]
+        <Text>
+        </Text>
+        </Pressable>
+        </View>)
+}
