f227d28582f318af2a35fcc5d7a334e24bcfe693
Pablos <pablo.alves@nave.org.br>
2026-05-26 15:22:49 -0300

    mudei só um pouco

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -1,5 +1,56 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
+Crie e exporte por padrão um componente chamado
+ "AtvBotaoAcaoInterna" que não recebe nenhum argumento. 
+ Este componente deve exibir uma <View> com dois elementos:
+  um <Text> com o texto "Clique abaixo", e um <Pressable> 
+  com o texto "Clique aqui" que ao ser clicado exibe um alerta
+   com o texto "Fui clicado!"
 */
+
+
+import { View, Text, Pressable, Alert, StyleSheet }
+ from 'react-native';
+
+const estilos = StyleSheet.create({
+  texto: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+
+  botao: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 7,
+  },
+
+  textoBotao: {
+    color: 'white',
+    fontSize: 20,
+  }
+});
+
+export default function AtvBotaoAcaoInterna() {
+
+  function mostrarAlerta() {
+    Alert.alert('FUI CLICADOOO!');
+  }
+
+  return (
+    <View>
+      <Text style={estilos.texto}>
+        Clique abaixo
+      </Text>
+
+      <Pressable
+        style={estilos.botao}
+        onPress={mostrarAlerta}
+      >
+        <Text style={estilos.textoBotao}>
+          Clique aqui
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
