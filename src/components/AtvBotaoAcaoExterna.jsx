6e9f0782f019a3c1c23460665926c7ad7e48cd10
Mike <izabella.cristine@nave.org.br>
2026-05-27 13:43:36 +0000

    Tentativa da questão 1

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -3,3 +3,18 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+import root from '../styles/main';
+
+export default function AtvBotaoAcaoExterna({comando}){
+
+    const comando(){
+
+    }
+    return(
+        <View>
+            <Text> {comando} </Text>
+        </View>
+    )
+}
