d20e5f505a1afd9a6a17a4dc29aa7652db4837be
Manuella Siqueira <manuella.siqueira@nave.org.br>
2026-05-31 17:08:31 -0300

    Componentes

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -1,22 +1,53 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado 
-"AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <View> 
-com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto 
-"Clique aqui" que ao ser clicado 
+
+Crie e exporte por padrão um componente chamado
+"AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <View>
+com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto
+"Clique aqui" que ao ser clicado
 exibe um alerta com o texto "Fui clicado!"
 */
 
-import { View, Pressable, Text, Alert} from 'react-native';
+
+import { View, Pressable, Text, Alert, StyleSheet} from 'react-native';
 import root from '../styles/main';
 
+
+const estiloBotao = StyleSheet.create({
+    botao: {
+         backgroundColor: 'pink',
+         borderRadius:'10px',
+         padding: '5px'
+         
+    },
+  texto1: {
+    color: 'white',
+    fontSize: "15px",
+    fontWeight:'bold',
+    textAlign:'center'
+    },
+    texto2: {
+    color: 'purple',
+    fontSize: "15px",
+    fontWeight:'bold',
+   textAlign:'center'
+
+
+    }
+}
+)
 export default function AtvBotaoAcaoInterna ({}) {
     return(
         <View>
-            <Text>Clique abaixo</Text>
-            <Pressable
-            On Press = {() => Alert.alert('Fui Clicado!')}>
+            <Text style = {estiloBotao.texto1}>Clique abaixo</Text>
+
+
+            <Pressable style = {estiloBotao.botao}
+             onPress = {() => window.alert('Fui Clicado!')}>
+
+
+                <Text  style = {estiloBotao.texto2}>Clique aqui</Text>
             </Pressable>
         </View>
     )
