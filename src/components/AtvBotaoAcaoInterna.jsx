0d0b716d1cb69d81b8816262be2eb7831b363734
Mike <izabella.cristine@nave.org.br>
2026-06-01 00:49:28 +0000

    Atv 2 completa

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -3,3 +3,38 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
+
+import { View, Pressable, Text, StyleSheet, Alert } from 'react-native';
+import root from '../styles/main';
+
+const estilo = StyleSheet.create({
+    div:{
+        backgroundColor:root.colors.azul,
+        padding:root.spacings.paddingG,
+        color:root.colors.background,
+        margin:root.spacings.paddingM,
+    },
+    botao:{
+        padding:root.spacings.paddingM,
+        color:root.colors.texto,
+        backgroundColor:root.colors.primariaEscura,
+    },
+    texto:{
+        color:root.colors.texto,
+    }
+})
+
+export default function AtvBotaoAcaoInterna({}){
+    return (
+        <View style={estilo.div}>
+            <Text sytle={estilo.texto}>
+                Clique abaixo
+            </Text>
+            <Pressable onPress={() => alert('Fui clicado!')} style={estilo.botao}>
+                <Text style={estilo.texto}> 
+                    Clique aqui 
+                </Text>
+            </Pressable>
+        </View>
+    )
+}
