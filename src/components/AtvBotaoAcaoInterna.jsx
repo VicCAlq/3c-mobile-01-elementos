88fbfa2cbd453dcab3b3b231ab6af8c4b9430e1c
marapessoa06 <samara.pessoa@nave.org.br>
2026-06-08 23:57:35 -0300

    attv

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -1,5 +1,25 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
+Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento
+. Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo",
+ e um <Pressable> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto 
+ "Fui clicado!"
 */
+
+import { View, Text, StyleSheet, Pressable, Alert } from 'react-native';
+
+export default function AtvBotaoAcaoInterna() {
+    return (
+        <View>
+<Text>
+    Clique abaixo
+</Text>
+<Pressable onPress={() => window.alert( "Fui clicado!")}>
+    <Text>
+        Clique aqui
+    </Text>
+</Pressable>
+        </View>
+    )
+}
