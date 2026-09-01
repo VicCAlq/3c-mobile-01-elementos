fa0fa2d20e1d89598875179a5f1d41f5e5c271ae
JonhPerter07 <joaopedro.rodrigues@nave.org.br>
2026-09-01 10:24:58 -0300

    Update AtvBotaoAcaoInterna.jsx
    feito.recuperação

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -3,3 +3,19 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <View> com dois elementos: um <Text> com o texto "Clique abaixo", e um <Pressable> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
+import { View, Text, StyleSheet, Pressable, Alert } from 'react-native';
+
+export default function AtvBotaoAcaoInterna() {
+    return (
+        <View>
+<Text>
+    Clique abaixo
+</Text>
+<Pressable onPress={() => window.alert( "Fui clicado!")}>
+    <Text>
+        Clique aqui
+    </Text>
+</Pressable>
+        </View>
+    )
+}
